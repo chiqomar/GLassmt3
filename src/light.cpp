e8657feea1c4b35d05a3c37a870daa9cc7387fad
chiqomar <chiqomar@gmail.com>
2015-05-17 13:56:47 -0400

    GOGOGOGOGO

---
+++ src/light.cpp
@@ -0,0 +1,174 @@
+<<<<<<< HEAD
+/*
+ * light.cpp
+ *
+ *  Created on: Dec 17, 2014
+ *      Author: nbingham
+ */
+
+#include "light.h"
+#include "object.h"
+#include "canvas.h"
+=======
+#include "light.h"
+#include "object.h"
+#include "opengl.h"
+>>>>>>> 5a28a44aa4eed5b7638f637d20a7bae5fa36dd5e
+
+lighthdl::lighthdl()
+{
+	model = NULL;
+	type = "light";
+}
+
+lighthdl::lighthdl(const vec3f &ambient, const vec3f &diffuse, const vec3f &specular)
+{
+	this->ambient = ambient;
+	this->diffuse = diffuse;
+	this->specular = specular;
+	model = NULL;
+	type = "light";
+}
+
+lighthdl::~lighthdl()
+{
+
+}
+
+directionalhdl::directionalhdl() : lighthdl(white*0.1f, white*0.5f, white)
+{
+	type = "directional";
+}
+
+directionalhdl::directionalhdl(const vec3f &direction, const vec3f &ambient, const vec3f &diffuse, const vec3f &specular) : lighthdl(ambient, diffuse, specular)
+{
+	type = "directional";
+}
+
+directionalhdl::~directionalhdl()
+{
+
+}
+
+void directionalhdl::update()
+{
+	if (canvas != NULL && model != NULL)
+	{
+		canvas->translate(model->position);
+		canvas->rotate(model->orientation[0], vec3f(1.0, 0.0, 0.0));
+		canvas->rotate(model->orientation[1], vec3f(0.0, 1.0, 0.0));
+		canvas->rotate(model->orientation[2], vec3f(0.0, 0.0, 1.0));
+
+		canvas->update_normal_matrix();
+		direction = canvas->matrices[canvashdl::normal_matrix]*vec4f(0.0, 0.0, -1.0, 0.0);
+
+		canvas->rotate(-model->orientation[2], vec3f(0.0, 0.0, 1.0));
+		canvas->rotate(-model->orientation[1], vec3f(0.0, 1.0, 0.0));
+		canvas->rotate(-model->orientation[0], vec3f(1.0, 0.0, 0.0));
+		canvas->translate(-model->position);
+	}
+}
+
+void directionalhdl::apply(string name, GLuint program)
+{
+	/* TODO Assignment 3: Pass all necessary uniforms to the shaders for the directional light.
+	 */
+}
+
+pointhdl::pointhdl() : lighthdl(white*0.1f, white*0.5f, white)
+{
+	this->attenuation = vec3f(1.0, 0.14, 0.7);
+	type = "point";
+}
+
+pointhdl::pointhdl(const vec3f &position, const vec3f &attenuation, const vec3f &ambient, const vec3f &diffuse, const vec3f &specular) : lighthdl(ambient, diffuse, specular)
+{
+	this->attenuation = attenuation;
+	type = "point";
+}
+
+pointhdl::~pointhdl()
+{
+
+}
+
+void pointhdl::update(canvashdl *canvas)
+{
+	if (canvas != NULL && model != NULL)
+	{
+		canvas->translate(model->position);
+		canvas->rotate(model->orientation[0], vec3f(1.0, 0.0, 0.0));
+		canvas->rotate(model->orientation[1], vec3f(0.0, 1.0, 0.0));
+		canvas->rotate(model->orientation[2], vec3f(0.0, 0.0, 1.0));
+
+		vec4f p = canvas->matrices[canvashdl::modelview_matrix]*vec4f(0.0, 0.0, 0.0, 1.0);
+		position = p(0,3)/p[3];
+
+		canvas->rotate(-model->orientation[2], vec3f(0.0, 0.0, 1.0));
+		canvas->rotate(-model->orientation[1], vec3f(0.0, 1.0, 0.0));
+		canvas->rotate(-model->orientation[0], vec3f(1.0, 0.0, 0.0));
+		canvas->translate(-model->position);
+	}
+}
+
+void pointhdl::apply(string name, GLuint program)
+{
+	/* TODO Assignment 3: Pass all necessary uniforms to the shaders for point lights.
+	 */
+}
+
+spothdl::spothdl() : lighthdl(white*0.1f, white*0.5f, white)
+{
+	this->attenuation = vec3f(1.0, 0.14, 0.7);
+	this->cutoff = 0.5;
+	this->exponent = 1.0;
+	type = "spot";
+}
+
+spothdl::spothdl(const vec3f &attenuation, const float &cutoff, const float &exponent, const vec3f &ambient, const vec3f &diffuse, const vec3f &specular) : lighthdl(ambient, diffuse, specular)
+{
+	this->attenuation = attenuation;
+	this->cutoff = cutoff;
+	this->exponent = exponent;
+	type = "spot";
+}
+
+spothdl::~spothdl()
+{
+
+}
+
+<<<<<<< HEAD
+void spothdl::update(canvashdl *canvas)
+{
+	if (canvas != NULL && model != NULL)
+	{
+		canvas->translate(model->position);
+		canvas->rotate(model->orientation[0], vec3f(1.0, 0.0, 0.0));
+		canvas->rotate(model->orientation[1], vec3f(0.0, 1.0, 0.0));
+		canvas->rotate(model->orientation[2], vec3f(0.0, 0.0, 1.0));
+
+		vec4f p = canvas->matrices[canvashdl::modelview_matrix]*vec4f(0.0, 0.0, 0.0, 1.0);
+		position = p(0,3)/p[3];
+		canvas->update_normal_matrix();
+		direction = canvas->matrices[canvashdl::normal_matrix]*vec4f(0.0, 0.0, -1.0, 0.0);
+
+		canvas->rotate(-model->orientation[2], vec3f(0.0, 0.0, 1.0));
+		canvas->rotate(-model->orientation[1], vec3f(0.0, 1.0, 0.0));
+		canvas->rotate(-model->orientation[0], vec3f(1.0, 0.0, 0.0));
+		canvas->translate(-model->position);
+	}
+}
+
+void spothdl::update()
+{
+	/* TODO Assignment 2: Update both the direction and position of the light using the position and orientation
+	 * of the attached model. See above.
+	 */
+}
+
+void spothdl::apply(string name, GLuint program)
+{
+	/* TODO Assignment 3: Pass all necessary uniforms to the shaders for spot lights.
+	 */
+}
---
+++ src/material.cpp
@@ -0,0 +1,209 @@
+#include "material.h"
+#include "light.h"
+#include "lodepng.h"
+
+GLuint whitehdl::vertex = 0;
+GLuint whitehdl::fragment = 0;
+GLuint whitehdl::program = 0;
+
+GLuint gouraudhdl::vertex = 0;
+GLuint gouraudhdl::fragment = 0;
+GLuint gouraudhdl::program = 0;
+
+GLuint brickhdl::vertex = 0;
+GLuint brickhdl::fragment = 0;
+GLuint brickhdl::program = 0;
+
+GLuint texturehdl::vertex = 0;
+GLuint texturehdl::fragment = 0;
+GLuint texturehdl::program = 0;
+GLuint texturehdl::texture = 0;
+
+extern string working_directory;
+
+materialhdl::materialhdl()
+{
+	type = "material";
+}
+
+materialhdl::~materialhdl()
+{
+}
+
+gouraudhdl::gouraudhdl()
+{
+	type = "gouraud";
+	emission = vec3f(0.0, 0.0, 0.0);
+	ambient = vec3f(0.1, 0.1, 0.1);
+	diffuse = vec3f(1.0, 1.0, 1.0);
+	specular = vec3f(1.0, 1.0, 1.0);
+	shininess = 1.0;
+
+	if (vertex == 0 && fragment == 0 && program == 0)
+	{
+		/* TODO Assignment 3: Load and link the shaders. Keep in mind that vertex, fragment,
+		 * and program are static variables meaning they are *shared across all instances of
+		 * this class. So you only have to initialize them once when the first instance of
+		 * the class is created.
+		 */
+	}
+}
+
+gouraudhdl::~gouraudhdl()
+{
+
+}
+
+void gouraudhdl::apply(const vector<lighthdl*> &lights)
+{
+	// TODO Assignment 3: Apply the shader program and pass it the necessary uniform values
+}
+
+materialhdl *gouraudhdl::clone() const
+{
+	gouraudhdl *result = new gouraudhdl();
+	result->type = type;
+	result->emission = emission;
+	result->ambient = ambient;
+	result->diffuse = diffuse;
+	result->specular = specular;
+	result->shininess = shininess;
+	return result;
+}
+
+phonghdl::phonghdl()
+{
+	type = "phong";
+	emission = vec3f(0.0, 0.0, 0.0);
+	ambient = vec3f(0.1, 0.1, 0.1);
+	diffuse = vec3f(1.0, 1.0, 1.0);
+	specular = vec3f(1.0, 1.0, 1.0);
+	shininess = 1.0;
+
+	if (vertex == 0 && fragment == 0 && program == 0)
+	{
+		/* TODO Assignment 3: Load and link the shaders. Keep in mind that vertex, fragment,
+		 * and program are static variables meaning they are *shared across all instances of
+		 * this class. So you only have to initialize them once when the first instance of
+		 * the class is created.
+		 */
+	}
+}
+
+phonghdl::~phonghdl()
+{
+
+}
+
+void phonghdl::apply(const vector<lighthdl*> &lights)
+{
+	// TODO Assignment 3: Apply the shader program and pass it the necessary uniform values
+}
+
+materialhdl *phonghdl::clone() const
+{
+	phonghdl *result = new phonghdl();
+	result->type = type;
+	result->emission = emission;
+	result->ambient = ambient;
+	result->diffuse = diffuse;
+	result->specular = specular;
+	result->shininess = shininess;
+	return result;
+}
+
+whitehdl::whitehdl()
+{
+	type = "white";
+
+	if (vertex == 0 && fragment == 0 && program == 0)
+	{
+		/* TODO Assignment 3: Load and link the shaders. Keep in mind that vertex, fragment,
+		 * and program are static variables meaning they are *shared across all instances of
+		 * this class. So you only have to initialize them once when the first instance of
+		 * the class is created.
+		 */
+	}
+}
+
+whitehdl::~whitehdl()
+{
+
+}
+
+void whitehdl::apply(const vector<lighthdl*> &lights)
+{
+	glUseProgram(program);
+}
+
+materialhdl *whitehdl::clone() const
+{
+	whitehdl *result = new whitehdl();
+	result->type = type;
+	return result;
+}
+
+brickhdl::brickhdl()
+{
+	type = "brick";
+
+	if (vertex == 0 && fragment == 0 && program == 0)
+	{
+		/* TODO Assignment 3: Load and link the shaders. Keep in mind that vertex, fragment,
+		 * and program are static variables meaning they are *shared across all instances of
+		 * this class. So you only have to initialize them once when the first instance of
+		 * the class is created.
+		 */
+	}
+}
+
+brickhdl::~brickhdl()
+{
+
+}
+
+void brickhdl::apply(const vector<lighthdl*> &lights)
+{
+	// TODO Assignment 3: Apply the shader program and pass it the necessary uniform values
+}
+
+materialhdl *brickhdl::clone() const
+{
+	brickhdl *result = new brickhdl();
+	result->type = type;
+	return result;
+}
+
+texturehdl::texturehdl()
+{
+	type = "texture";
+
+	shininess = 1.0;
+
+	if (vertex == 0 && fragment == 0 && program == 0)
+	{
+		/* TODO Assignment 3: Load and link the shaders and load the texture Keep in mind that vertex, fragment,
+		 * and program are static variables meaning they are *shared across all instances of
+		 * this class. So you only have to initialize them once when the first instance of
+		 * the class is created.
+		 */
+	}
+}
+
+texturehdl::~texturehdl()
+{
+
+}
+
+void texturehdl::apply(const vector<lighthdl*> &lights)
+{
+	// TODO Assignment 3: Apply the shader program and pass it the necessary uniform values
+}
+
+materialhdl *texturehdl::clone() const
+{
+	texturehdl *result = new texturehdl();
+	result->type = type;
+	result->shininess = shininess;
+	return result;
+}
